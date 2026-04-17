3170f490806062c9f738f13c5694df7669619a89
Justin Ren <justinrenprf@gmail.com>
2026-04-17 10:57:35 -0400

    Update pileup_array.sh

--- slurm array/pileup_array.sh
+++ slurm array/pileup_array.sh
@@ -3,13 +3,16 @@
 #SBATCH -A r00302
 #SBATCH -p general
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=64G
+#SBATCH --mem=32G
 #SBATCH --time=48:00:00
-#SBATCH --array=2-2
+#SBATCH --array=1-10
 #SBATCH --output=logs/modkit_pileup_%A_%a.out
 #SBATCH --error=logs/modkit_pileup_%A_%a.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=justren@iu.edu
+
+module load samtools
+module load htslib
 
 set -e
 set -o pipefail
@@ -18,9 +21,9 @@ mkdir -p logs
 
 REF=/geode2/home/u040/justren/Quartz/fasta_ref/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna
 BAM_BASE=/N/project/ICMH/Projects/project_clock/data/filtered_reversed_bams
-OUTPUT_BASE=/N/project/ICMH/Projects/project_clock/data/modkit_6mA_pileup
-MODKIT=/geode2/home/u040/justren/Quartz/modkit  # adjust to your modkit binary path
-MANIFEST=manifest.csv
+OUTPUT_BASE=/N/project/ICMH/Projects/project_clock/data/MODKIT_PILEUP_6mA
+MODKIT=/N/slate/justren/modkit_dir/dist_modkit_v0.5.1_8fa79e3
+MANIFEST=/N/slate/justren/shell_scripts/manifest760_597.csv
 
 # Parse manifest row for this task
 SAMPLE=$(awk  -F',' -v row="${SLURM_ARRAY_TASK_ID}" 'NR==row {print $1}' ${MANIFEST})
@@ -51,11 +54,12 @@ mkdir -p "$(dirname ${OUTPUT_BED})"
 # Run modkit pileup (6mA only)
 ${MODKIT} pileup \
     "${INPUT_BAM}" \
-    "${OUTPUT_BED}" \
-    --ref "${REF}" \
+    - \
     --motif A 0 \
+    --ref "${REF}" \
     --threads ${SLURM_CPUS_PER_TASK} \
-    --log-filepath "logs/modkit_${SAMPLE}_${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}.log"
+    --log-filepath "logs/modkit_${SAMPLE}_${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}.txt" \
+    | bgzip -c > ${OUTPUT_BED}.gz
 
 echo "Finished at : $(date)"
-echo "Output size : $(du -h ${OUTPUT_BED})"
+echo "Output size : $(du -h ${OUTPUT_BED}.gz)"
